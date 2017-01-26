b3d212630c74ed58cda07b1c2d6102d05fc98516
Nadine Brandstetter <nadine.brandstetter@hotmail.com>
2017-01-26 18:35:59 +0100

    changed some comments

--- ce-bay_client/src/main/java/at.jku.ce.bay.app/App.java
+++ ce-bay_client/src/main/java/at.jku.ce.bay.app/App.java
@@ -4,13 +4,17 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import at.jku.ce.bay.utils.CEBayHelper;
 import client.Client;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
+
+/*
+* CE ÜBUNGSPROJEKT --> APP FOR CLIENT
+* Nadine Brandstetter & Michael-Christian Ortner
+*/
 
 public class App {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
